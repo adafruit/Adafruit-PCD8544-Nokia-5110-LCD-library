dbfd118a4e7a0c5bc963bcff854ed74ac3bef37f
KenjutsuGH <pjakruger@yahoo.com>
2018-07-21 15:46:32 +0200

    Added support for STM32duino

--- examples/pcdtest/pcdtest.ino
+++ examples/pcdtest/pcdtest.ino
@@ -26,7 +26,11 @@ All text above, and the splash screen must be included in any redistribution
 // pin 5 - Data/Command select (D/C)
 // pin 4 - LCD chip select (CS)
 // pin 3 - LCD reset (RST)
-Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
+ #if defined (__STM32F1__)
+   Adafruit_PCD8544 display = Adafruit_PCD8544(PB12, PA8, PB15, PB14, PB13);
+ #else
+   Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
+ #endif
 
 // Hardware SPI (faster, but must use certain hardware pins):
 // SCK is LCD serial clock (SCLK) - this is pin 13 on Arduino Uno
@@ -34,7 +38,15 @@ Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
 // pin 5 - Data/Command select (D/C)
 // pin 4 - LCD chip select (CS)
 // pin 3 - LCD reset (RST)
-// Adafruit_PCD8544 display = Adafruit_PCD8544(5, 4, 3);
+// For the STM32F1:
+//   MOSI - on PA7 (Maple Mini: also known as pin 4)
+//   SCK  - on PA5 (Maple Mini: also known as pin 6)
+//
+//#if defined (__STM32F1__)
+  //Adafruit_PCD8544 display = Adafruit_PCD8544(PB15, PB14, PB13);
+//#else
+  //Adafruit_PCD8544 display = Adafruit_PCD8544(5, 4, 3);
+//#endif
 // Note with hardware SPI MISO and SS pins aren't used but will still be read
 // and written to during SPI transfer.  Be careful sharing these pins!
 
