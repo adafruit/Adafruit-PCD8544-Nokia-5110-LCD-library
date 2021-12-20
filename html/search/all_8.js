d54a6dbc3f044930461a177853bc64a139258cd1
Doxygen CI <ci-arduino@invalid>
2021-12-20 19:24:26 +0000

    Deploy docs to GitHub Pages from commit d5a105e41c
    Commit: d5a105e41cddd3db7fe1c4f28679cfa8ee69562c
    GitHub Actions run: 1603627083

--- html/search/all_8.js
+++ html/search/all_8.js
@@ -3,6 +3,6 @@ var searchData=
   ['scroll',['scroll',['../class_adafruit___p_c_d8544.html#a0ee1a57e50a5a35c69be0386c0243ee9',1,'Adafruit_PCD8544']]],
   ['setbias',['setBias',['../class_adafruit___p_c_d8544.html#a657a51185881e20d747c2993fd0aa5e6',1,'Adafruit_PCD8544']]],
   ['setcontrast',['setContrast',['../class_adafruit___p_c_d8544.html#a117a5ab05a6d9c769a4dccde1a31ba68',1,'Adafruit_PCD8544']]],
-  ['setpixel',['setPixel',['../class_adafruit___p_c_d8544.html#a0690349dab5e8936959650c9d0f7aa30',1,'Adafruit_PCD8544']]],
+  ['setpixel',['setPixel',['../class_adafruit___p_c_d8544.html#a6e603ebd630133fc5751e41165d64b63',1,'Adafruit_PCD8544']]],
   ['setreinitinterval',['setReinitInterval',['../class_adafruit___p_c_d8544.html#a1d3e0c6138e184efddb6ab6c1073bfc3',1,'Adafruit_PCD8544']]]
 ];
